1cad0f4b6a616b088dc9eab77a7b5f81d97e0371
Andreas Wilke <wilke@mcs.anl.gov>
2017-03-01 09:25:13 -0600

    Creating attributes for shock node

--- scripts/processReceipt.pl
+++ scripts/processReceipt.pl
@@ -11,6 +11,9 @@ use Net::FTP;
 
 my $file = shift @ARGV ;
 
+my $upload  = 1 ;
+my $debug   = 0 ;
+
 print $file , "\n";
 
 my $receipt = new Receipt();
@@ -19,7 +22,68 @@ print join "\t" , "Success:" , $receipt->success() || "undef" , "\n";
 print join "\t" , "Submission:" , $receipt->submission , "\n";
 
 
+if ($upload) {
+  
+  my $json = JSON->new ;
+  my $attributes = {
+    project_id    => $receipt->xml->{STUDY}->{alias} ,
+    submission_id => $receipt->xml->{SUBMISSION}->{alias},
+    type          => 'EBI Submission Receipt' ,
+    receipt       => $receipt->xml ,
+    db_xref       => {
+      submission  => $receipt->xml->{SUBMISSION} ,
+      libraries   => $receipt->xml->{EXPERIMENT} ,
+      project     => $receipt->xml->{STUDY} ,
+      samples     => $receipt->xml->{SAMPLE} ,
+      files       => $receipt->xml->{RUN} , # stage
+      
+    }
+  };
+  #$attributes = $json->encode($receipt->xml) ;
+  
+  print Dumper $attributes , "\n" ; 
+  
+}
 
+
+
+
+sub get_md5{
+  my ($file)  = @_ ;
+  
+  my $md5bin =  `which md5sum `;
+  chomp $md5bin ;
+  print "TEST:\n" , $md5bin , "END\n";
+  
+  # which md5 script, linux versus mac
+  
+  unless($md5bin){
+    $md5bin =  `which md5`;
+    chomp $md5bin ;
+    unless($md5bin){
+      print STDERR "Can't compute md5 sum. Can't find md5 tool.\n";
+      exit;
+    }
+    else{
+      $md5bin .= " -r" ;
+    }
+  }
+  
+  my $result     = `$md5bin $file` ;  
+  my ($md5 , $f) = split " " , $result ;
+
+  unless($md5){
+    print STDERR "Can't compute md5 for $file\n" ;
+    exit;
+  }
+  unless($f eq $file){
+    print STDERR "Something wrong, computed md5 for wrong file:\n($file\t$f)\n";
+    exit;
+  }  
+  
+  print STDERR "MD5 for local files:\t" , $result , "\n" if ($debug);
+  return $md5 ;
+} 
 
 
 
